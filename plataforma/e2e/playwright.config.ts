c34668947caf8a057a7405a88fbca9a848555eab
Curso Ciber Platform <redp41732@gmail.com>
2026-07-16 12:53:41 -0500

    fix: deshabilitar HTTP2 en Chromium para E2E en QA/prod

--- plataforma/e2e/playwright.config.ts
+++ plataforma/e2e/playwright.config.ts
@@ -33,7 +33,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--disable-http2'],
+        },
+      },
     },
     {
       name: 'firefox',
